e301ceabb40412400502af4d2ae8aecae2140645
Sara <sara@quesu.local>
2026-04-09 19:32:34 +0200

    Initial commit: DOM exercises

---
+++ exercise1/index.js
@@ -0,0 +1,18 @@
+const button = document.querySelector('.showme');
+console.log(button);
+
+const h1 = document.querySelector('#pillado');
+console.log(h1);
+
+const p = document.querySelector('p');
+console.log(p);
+
+const pokemon = document.querySelector('.pokemon');
+console.log(pokemon);
+
+const testMe = document.querySelectorAll('[data-function="testMe"]');
+console.log(testMe);
+
+const thirdCharacter = document.querySelectorAll('[data-function="testMe"]')[2];
+console.log(thirdCharacter);
+
